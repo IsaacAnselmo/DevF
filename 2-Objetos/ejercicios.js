0b751595fabf79d1cff3f262f300920bf01d28ea
Isaacz <isaac.anselmo.z@gmail.com>
2020-06-25 22:13:50 -0500

    Add files via upload

--- 2-Objetos/ejercicios.js
+++ 2-Objetos/ejercicios.js
@@ -1,16 +1,42 @@
 const myPenguin ={
     name:"Mr. Penguin",
     origin:"Top Wing",
-    note:"Penny's dad"
+    note:"Penny's dad",
+
+    getName()
+    {
+        return this.name ;
+    }
 
 };
 
-console.log(`Hola, soy un pingüino y mi nombre es ${myPenguin.name}`);
+console.log(`Hola, soy un pingüino y mi nombre es ${myPenguin.getName()}`);
 
 myPenguin.puedeVolar=false;
 
 myPenguin.graznar = function(){
-    console.log("kaww kaww!!");
+    return "kaww kaww!!";
+}
+//punto 5
+myPenguin.saludar=function()
+{
+    return `Hola, soy un pingüino y mi nombre es ${this.getName()}`;
+}
+//punto 6
+myPenguin.name="Señor Pingu";
+
+console.log(`${myPenguin.saludar()}`);
+//punto 7
+myPenguin.volar=function()
+{
+    if(this.puedeVolar)
+        return `Puedo volar`
+    else
+        return `No puedo volar :(`
 }
 
-myPenguin.graznar();
+console.log(`${myPenguin.volar()}`);
+//punto 8
+myPenguin.puedeVolar=true;
+
+console.log(`${myPenguin.volar()}`);
